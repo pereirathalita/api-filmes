f0eaf2239d8b551733d03608999b5788d84f185f
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-09-11 21:49:03 -0300

    feat: cards filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,22 @@
 function Home() {
     return (
         <div>
-        <h1>Hello, Word!</h1>
+        <h1>Movies</h1>
+        <ul>
+            <li>
+                <img src="https://www.sonypictures.com/movies/spiderman3" alt=""/>
+                <span>Spider Man</span>
+            </li>
+            <li>
+                <img src="" alt=""/>
+                <span></span>
+            </li>
+            <li>
+                <img src="" alt=""/>
+                <span></span>
+            </li>
+        </ul>
+        
         </div>
     )
 }
